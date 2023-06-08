46f559a3eb898832d270a06fb91866c369591903
Xyrlan <lobatopedro8@gmail.com>
2023-06-08 13:30:15 -0300

    fix text-size

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           <Image className='absolute group-hover:opacity-100 opacity-0 duration-300' src={'/images/icon-view.svg'} width={48} height={48} alt='icon-view' />
         </div>
 
-        <h1 className='text-white hover:text-cyan duration-200 cursor-pointer text-lg my-6 font-semibold'>Equilibrium #3429</h1>
+        <h1 className='text-white hover:text-cyan duration-200 cursor-pointer text-xl my-6 font-semibold'>Equilibrium #3429</h1>
         <p className='text-soft-blue mb-6'>Our Equilibirum collection promotes balance and calm.</p>
 
         <div className='flex justify-between border-b border-line my-2 pb-4'>
